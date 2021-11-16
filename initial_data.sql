6893d1b2a92eb1b38b121107608b51d2ca9956ec
rab85714 <46548080+rab85714@users.noreply.github.com>
2021-11-15 19:27:39 -0500

    adding more to sql file

--- initial_data.sql
+++ initial_data.sql
@@ -21,3 +21,28 @@ CREATE TABLE IF NOT EXISTS user (
   isAdmin tinyint(1) NOT NULL,
   PRIMARY KEY (id)
 );
+
+CREATE TABLE cart (
+  id int(11) NOT NULL,
+  itemId int(11) NOT NULL,
+  PRIMARY KEY(id) REFERENCES user(id),
+  FOREIGN KEY(itemId) REFERENCES menuItem(id)
+);
+
+CREATE TABLE orderhistory (
+ id int(11) NOT NULL,
+ itemId int(11) NOT NULL,
+ -- Date of Purchase
+ dop varchar(255) NOT NULL,
+ PRIMARY KEY(id),
+ FOREIGN KEY(id) references user(id),
+ FOREIGN KEY(itemId) references menuItem(id)
+);
+
+INSERT INTO user VALUES
+(1, 'admin@mail.com', 'password', 'Master', 'Admin', '555-555-5555', 1),
+(2, 'user1@mail.com', 'pwd1', 'Average', 'Joe', '555-666-6666', 0);
+
+INSERT INTO menuItem VALUES
+(1, 'Pasta', 12.00, 'Angel hair pasta with freshly churned butter and salt', 0)
+(2, 'Pizza', 13.00, 'Gluten free, dairy free, red dye free, fun free pizza', 1)
