daa645dd1e076100e4754946ff08709d0b75fcf9
rab85714 <46548080+rab85714@users.noreply.github.com>
2021-11-15 19:03:14 -0500

    trying again again

--- initial_data.sql
+++ initial_data.sql
@@ -2,22 +2,22 @@ CREATE DATABASE IF NOT EXISTS restaurant;
 
 USE restaurant;
 
-CREATE TABLE IF NOT EXISTS menuItem (
-'id' int(11) NOT NULL,
-'name' varchar(255) NOT NULL,
-'price' decimal(10,2) NOT NULL,
-'description' varchar(255),
-'inStock' tinyint(1) NOT NULL,
-PRIMARY KEY('id')
+CREATE TABLE IF NOT EXISTS 'menuItem' (
+  'id' int(11) NOT NULL,
+  'name' varchar(255) NOT NULL,
+  'price' decimal(10,2) NOT NULL,
+  'description' varchar(255),
+  'inStock' tinyint(1) NOT NULL,
+  PRIMARY KEY('id')
 );
 
-CREATE TABLE IF NOT EXISTS user (
-`id` int(11) NOT NULL AUTO_INCREMENT,
-`email` varchar(255) NOT NULL,
-`password` varchar(255) NOT NULL,
-`firstName` varchar(255) NOT NULL,
-`lastName` varchar(255) NOT NULL,
-`phone` varchar(255) NOT NULL,
-`isAdmin` tinyint(1) NOT NULL,
-PRIMARY KEY (`id`)
+CREATE TABLE IF NOT EXISTS 'user' (
+  'id' int(11) NOT NULL AUTO_INCREMENT,
+  'email' varchar(255) NOT NULL,
+  'password' varchar(255) NOT NULL,
+  'firstName' varchar(255) NOT NULL,
+  'lastName' varchar(255) NOT NULL,
+  'phone' varchar(255) NOT NULL,
+  'isAdmin' tinyint(1) NOT NULL,
+  PRIMARY KEY ('id')
 );
