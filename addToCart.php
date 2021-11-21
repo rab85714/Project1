07a46c3e7e814d005997d3c75177ec139763150e
rab85714 <46548080+rab85714@users.noreply.github.com>
2021-11-21 17:19:58 -0500

    still not working

--- addToCart.php
+++ addToCart.php
@@ -2,12 +2,13 @@
   require('./database.php');
   session_start();
 
-  $itemId = filter_input(INPUT_POST, 'id');
+  $input = filter_input(INPUT_POST, 'id');
+  $itemID = $input['id'];
 
   $email = $_SESSION['email'];
 
   $queryItemsToAdd = 'SELECT * FROM menuitem WHERE menuitem.id = '$itemId'';
-  $queryCurrentUserID = 'SELECT 'id' FROM user WHERE user.email = '$email'';
+  $queryCurrentUserID = 'SELECT id FROM user WHERE user.email = '$email'';
 
   $userID = $db->query($queryCurrentUserID);
   $itemsToAdd = $db->query($queryItemsToAdd);
