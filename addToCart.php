07298aaaaa261a19d147941cfcd5d9cfb41ddaff
rab85714 <46548080+rab85714@users.noreply.github.com>
2021-11-21 16:55:10 -0500

    another typo

--- addToCart.php
+++ addToCart.php
@@ -4,7 +4,7 @@
 
   $id = filter_input(INPUT_POST, 'id');
 
-  $queryItemsToAdd = 'SELECT * FROM books WHERE id = '$id'';
+  $queryItemsToAdd = 'SELECT * FROM books WHERE id = $id';
   $email = $_SESSION['email'];
   $queryCurrentUserID = 'SELECT id FROM user WHERE email = $email';
 
