51daa62ade1eae533b8b3416f5a5a57ca5beb14b
rab85714 <46548080+rab85714@users.noreply.github.com>
2021-11-21 16:58:42 -0500

    and again

--- addToCart.php
+++ addToCart.php
@@ -3,14 +3,14 @@
   session_start();
 
   $id = filter_input(INPUT_POST, 'id');
+  $email = $_SESSION['email'];
 
   $queryItemsToAdd = 'SELECT * FROM books WHERE id = '$id'';
-  $email = $_SESSION['email'];
-  $queryCurrentUserID = 'SELECT id FROM user WHERE email = '$email'';
+  $queryCurrentUserID = 'SELECT id FROM user WHERE user.email = '$email'';
 
   $userID = $db->query($queryCurrentUserID);
   $itemsToAdd = $db->query($queryItemsToAdd);
 
-  $queryAlterCart = 'INSERT INTO cart VALUES ($userID, $itemsToAdd)';
+  $queryAlterCart = 'INSERT INTO cart VALUES ('$userID', '$itemsToAdd')';
   header("Location: menu.php");
 ?>
