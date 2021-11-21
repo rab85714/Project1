0932b0dc7c34a4565f1b808f713d7e378593cefa
rab85714 <46548080+rab85714@users.noreply.github.com>
2021-11-21 16:56:10 -0500

    workin on it

--- addToCart.php
+++ addToCart.php
@@ -4,9 +4,9 @@
 
   $id = filter_input(INPUT_POST, 'id');
 
-  $queryItemsToAdd = 'SELECT * FROM books WHERE id = $id';
+  $queryItemsToAdd = 'SELECT * FROM books WHERE id = '$id'';
   $email = $_SESSION['email'];
-  $queryCurrentUserID = 'SELECT id FROM user WHERE email = $email';
+  $queryCurrentUserID = 'SELECT id FROM user WHERE email = '$email'';
 
   $userID = $db->query($queryCurrentUserID);
   $itemsToAdd = $db->query($queryItemsToAdd);
