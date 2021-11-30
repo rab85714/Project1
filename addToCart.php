42961191d74568f561b74da5569707c5293959fd
rab85714 <46548080+rab85714@users.noreply.github.com>
2021-11-30 09:56:04 -0500

    trying

--- addToCart.php
+++ addToCart.php
@@ -3,6 +3,11 @@
   session_start();
 
   $query = 'INSERT INTO cart VALUES (1, 1)';
-  $info = $db->query($query);
-  header("Location: cart.php");
+  $info = mysqli_query($db, $query);
+  $resultCheck = mysqli_num_rows($info);
+
+  if ($resultCheck > 0){
+    header("Location: cart.php");
+  }
+  header("Location: logout.php");
 ?>
