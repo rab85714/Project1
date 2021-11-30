753f740eaba70566117b2fc741e2761d5d2a9ddb
rab85714 <46548080+rab85714@users.noreply.github.com>
2021-11-30 13:59:34 -0500

    ahhh

--- addToCart.php
+++ addToCart.php
@@ -5,9 +5,10 @@
   $itemId = filter_input(INPUT_POST, 'id');
   $userQuery = "SELECT * FROM user WHERE email = '{$_SESSION['email']}'";
   $user = $db->query($userQuery);
+  $userId = $user['id'];
   $menuItemIdQuery = "SELECT id FROM menuitem WHERE id = '{$itemId}'";
 
-  $queryInsert = "INSERT INTO cart VALUES ({$user['id'] + 0}, 1)";
+  $queryInsert = "INSERT INTO cart VALUES ($userId, 1)";
   $insert = $db->query($queryInsert);
 
   header('location: cart.php');
