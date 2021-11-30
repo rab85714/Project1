19dd11519d9e5328647729e4934ed1dfc6abc5d1
rab85714 <46548080+rab85714@users.noreply.github.com>
2021-11-30 15:21:37 -0500

    fun

--- addToCart.php
+++ addToCart.php
@@ -13,14 +13,13 @@
 <?php
   if (isset($_POST['add'])) {
     $itemId = filter_input(INPUT_POST, 'itemId');
-    print "itemId " . $itemId . "<br>";
     $email = $_SESSION['email'];
     $userQuery = "SELECT * FROM user WHERE email = :email";
     $userPrep = $db->prepare($userQuery);
     $userPrep->bindParam(':email', $email, PDO::PARAM_STR);
     $userId = $userPrep->execute();
 
-    $insertQuery = "INSERT INTO cart (id, itemId) VALUES (':userId', ':itemId')";
+    $insertQuery = "INSERT INTO cart (id, itemId) VALUES ($userId, $itemId)";
     $insert = $db->prepare($insertQuery);
     $insert->bindParam(':userId', $userId, PDO::PARAM_INT);
     $insert->bindParam(':itemId', $itemId, PDO::PARAM_INT);
--- initial_data.sql
+++ initial_data.sql
@@ -25,7 +25,6 @@ CREATE TABLE IF NOT EXISTS user (
 CREATE TABLE IF NOT EXISTS cart (
   id int(11) NOT NULL,
   itemId int(11) NOT NULL,
-  PRIMARY KEY(id),
   FOREIGN KEY(id) references user(id),
   FOREIGN KEY(itemId) references menuItem(id)
 );
