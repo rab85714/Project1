1acef02d113f65d4e58fea81d67fba77ac97b432
rab85714 <46548080+rab85714@users.noreply.github.com>
2021-11-30 13:52:33 -0500

    not working

--- addToCart.php
+++ addToCart.php
@@ -3,7 +3,7 @@
   session_start();
 
   $itemId = filter_input(INPUT_POST, 'id');
-  $userQuery = "SELECT * FROM user WHERE email = '{$_SESSION['email']}'";
+  $userQuery = "SELECT id FROM user WHERE email = '{$_SESSION['email']}'";
   $menuItemIdQuery = "SELECT id FROM menuitem WHERE id = '{$itemId}'";
 
   $queryInsert = "INSERT INTO cart VALUES ('{$userQuery['id']}', 1)";
