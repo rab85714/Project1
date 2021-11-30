6a1ddf3da94b7447a148708d943da6574b9ac6d7
rab85714 <46548080+rab85714@users.noreply.github.com>
2021-11-30 10:53:13 -0500

    reverting

--- addToCart.php
+++ addToCart.php
@@ -1,25 +1,14 @@
 <?php
   require('./database.php');
   session_start();
+
+  $itemId = filter_input(INPUT_POST, 'id');
+  if ($itemId = 1){
+    header("location: cart.php");
+  }
+  $userIdQuery = "SELECT id FROM user WHERE email = '{$_SESSION['email']}'";
+
+  $queryInsert = "INSERT INTO cart VALUES (1, '$itemId')";
+  $insert = $db->query($queryInsert);
+  header("Location: cart.php");
 ?>
-
-<!DOCTYPE html>
-
-<html lang="en">
-
-<head>
-</head>
-<body>
-    <?php
-      $itemId = filter_input(INPUT_POST, 'id');
-      if ($itemId > 0){
-          $userIdQuery = "SELECT id FROM user WHERE email = '{$_SESSION['email']}'";
-
-          $queryInsert = "INSERT INTO cart VALUES (1, '$itemId')";
-          $insert = $db->query($queryInsert);
-          header("Location: cart.php");
-      }
-    ?>
-    <p><?php echo $itemId ?></p>
-</body>
-</html>
