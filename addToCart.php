2640b0f35e1c2d9da39be36dedf9728bed4a370c
rab85714 <46548080+rab85714@users.noreply.github.com>
2021-11-30 10:46:03 -0500

    plz work

--- addToCart.php
+++ addToCart.php
@@ -3,9 +3,11 @@
   session_start();
 
   $itemId = filter_input(INPUT_POST, 'id');
-  $userIdQuery = "SELECT id FROM user WHERE email = '{$_SESSION['email']}'";
+  if ($itemId > 0){
+      $userIdQuery = "SELECT id FROM user WHERE email = '{$_SESSION['email']}'";
 
-  $queryInsert = "INSERT INTO cart VALUES (1, '$itemId')";
-  $insert = $db->query($queryInsert);
-  header("Location: cart.php");
+      $queryInsert = "INSERT INTO cart VALUES (1, '$itemId')";
+      $insert = $db->query($queryInsert);
+      header("Location: cart.php");
+  }
 ?>
