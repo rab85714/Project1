f3e11777132bbed382f77718cb6b73fce7199209
rab85714 <46548080+rab85714@users.noreply.github.com>
2021-11-30 10:36:15 -0500

    typo

--- addToCart.php
+++ addToCart.php
@@ -4,9 +4,9 @@
 
   $itemId = filter_input(INPUT_POST, 'id');
   $userIdQuery = "SELECT id FROM user WHERE email = '{$_SESSION['email']}'";
-  
+
 
   $queryInsert = "INSERT INTO cart VALUES ('{$userIdQuery}', '$itemId')";
-  $insert = $db->query($query);
+  $insert = $db->query($queryInsert);
   header("Location: cart.php");
 ?>
