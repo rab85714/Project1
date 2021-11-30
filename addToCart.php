642e6e92e7b8a158e4d4032917ba947a2c5b79ef
rab85714 <46548080+rab85714@users.noreply.github.com>
2021-11-30 14:23:19 -0500

    givin it a try

--- addToCart.php
+++ addToCart.php
@@ -3,12 +3,11 @@
   session_start();
 
   $itemId = filter_input(INPUT_POST, 'id');
-  $userQuery = "SELECT id FROM user WHERE email = '{$_SESSION['email']}'";
-  $userId = $db->query($userQuery);
+  $sessionId = $_SESSION['id'];
   $menuItemIdQuery = "SELECT id FROM menuitem WHERE id = '{$itemId}'";
 
   $queryInsert = "INSERT INTO cart (id, itemId) VALUES (':userId', ':itemId')";
-  $insert->bindParam(':userId', $userId, PDO::PARAM_INT);
+  $insert->bindParam(':userId', $sessionId, PDO::PARAM_INT);
   $insert->bindParam(':itemId', $itemId, PDO::PARAM_INT);
   $insert = $db->query($queryInsert);
 
