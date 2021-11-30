f8ddfab25ff93c0b061c719c737ad5912ea804cc
rab85714 <46548080+rab85714@users.noreply.github.com>
2021-11-30 10:50:10 -0500

    trying again

--- addToCart.php
+++ addToCart.php
@@ -1,15 +1,6 @@
 <?php
   require('./database.php');
   session_start();
-
-  $itemId = filter_input(INPUT_POST, 'id');
-  if ($itemId > 0){
-      $userIdQuery = "SELECT id FROM user WHERE email = '{$_SESSION['email']}'";
-
-      $queryInsert = "INSERT INTO cart VALUES (1, '$itemId')";
-      $insert = $db->query($queryInsert);
-      header("Location: cart.php");
-  }
 ?>
 
 <!DOCTYPE html>
@@ -19,6 +10,16 @@
 <head>
 </head>
 <body>
+    <?php
+      $itemId = filter_input(INPUT_POST, 'id');
+      if ($itemId > 0){
+          $userIdQuery = "SELECT id FROM user WHERE email = '{$_SESSION['email']}'";
+
+          $queryInsert = "INSERT INTO cart VALUES (1, '$itemId')";
+          $insert = $db->query($queryInsert);
+          header("Location: cart.php");
+      }
+    ?>
     <p><?php echo $itemId ?></p>
 </body>
 </html>
