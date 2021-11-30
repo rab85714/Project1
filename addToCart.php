702ee57adc11ca7e525ee9f6b795770f8ddcfaf4
rab85714 <46548080+rab85714@users.noreply.github.com>
2021-11-30 13:45:18 -0500

    another attempt

--- addToCart.php
+++ addToCart.php
@@ -6,8 +6,9 @@
   $userIdQuery = "SELECT id FROM user WHERE email = '{$_SESSION['email']}'";
   $menuItemIdQuery = "SELECT id FROM menuitem WHERE id = '{$itemId}'";
 
-  $queryInsert = "INSERT INTO cart VALUES (1, '$menuItemIdQuery')";
-  if ($db->query($queryInsert) === TRUE) {
-    header('location: cart.php');
-  }
+  $queryInsert = "INSERT INTO cart VALUES ('{$userIdQuery}', 1)";
+  $insert = $db->query($queryInsert);
+
+  header('location: cart.php');
+
 ?>
