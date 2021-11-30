233aa2681349fc694df5a8f2ce9e8647d06f2142
rab85714 <46548080+rab85714@users.noreply.github.com>
2021-11-30 15:33:57 -0500

    im stupid

--- addToCart.php
+++ addToCart.php
@@ -13,14 +13,14 @@
 <?php
   if (isset($_POST['add'])) {
     $itemIdRaw = filter_input(INPUT_POST, 'itemId');
-    $itemId = $itemIdRaw[0] * 1;
+    $itemId = $itemIdRaw[0];
     $email = $_SESSION['email'];
     $userQuery = "SELECT * FROM user WHERE email = :email";
     $userPrep = $db->prepare($userQuery);
     $userPrep->bindParam(':email', $email, PDO::PARAM_STR);
     $userId = $userPrep->execute();
 
-    $insertQuery = "INSERT INTO cart (id, itemId) VALUES ($userId, $itemId)";
+    $insertQuery = "INSERT INTO cart (id, itemId) VALUES (:userId, :itemId)";
     $insert = $db->prepare($insertQuery);
     $insert->bindParam(':userId', $userId, PDO::PARAM_INT);
     $insert->bindParam(':itemId', $itemId, PDO::PARAM_INT);
