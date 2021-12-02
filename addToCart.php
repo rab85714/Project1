a247880d1b7107e31f7625a8dae0aae995089d62
rab85714 <46548080+rab85714@users.noreply.github.com>
2021-12-02 14:04:05 -0500

    added quantity column to cart table

--- addToCart.php
+++ addToCart.php
@@ -20,7 +20,7 @@
     $userPrep->bindParam(':email', $email, PDO::PARAM_STR);
     $userId = $userPrep->execute();
 
-    $insertQuery = "INSERT INTO cart (id, itemId) VALUES (:userId, :itemId)";
+    $insertQuery = "INSERT INTO cart (id, itemId, quantity) VALUES (:userId, :itemId, 1)";
     $insert = $db->prepare($insertQuery);
     $insert->bindParam(':userId', $userId, PDO::PARAM_INT);
     $insert->bindParam(':itemId', $itemId, PDO::PARAM_INT);
