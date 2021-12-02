77f41c47f73748577e7bf9771de20a8476c88304
rab85714 <46548080+rab85714@users.noreply.github.com>
2021-12-02 13:33:30 -0500

    attempt to clean up cart

--- cart.php
+++ cart.php
@@ -7,8 +7,9 @@
     header("location: login.php");
   }
 
-  $queryCart = "SELECT * FROM cart, user, menuitem
-    WHERE user.email = '$email' AND user.id = cart.id AND cart.itemId = menuitem.id";
+// this query contains the following variables: id, itemId, name, and price
+  $queryCart = "SELECT user.id, cart.itemId, menuitem.name, menuitem.price FROM cart, user, menuitem
+                    WHERE user.email = '$email' AND user.id = cart.id AND cart.itemId = menuitem.id";
 
   $cart = $db->query($queryCart);
 ?>
@@ -51,32 +52,26 @@
 </header>
 <body>
   <div class="grid-container">
-        <table>
-
-           <form action="removeFromCart.php" method="post">
-
-            <tr><th></th><th>Title</th><th>Quantity</th><th>Price</th></tr>
-            <tr>
-                <?php foreach($cart as $item):?>
-
-                  <td><input type="number" placeholder = 1 min = 1 max = 10></td>
-                  <td>$<?php echo $item['price']?></td>
-                  <a><input type="hidden" name="itemId" value="<?php echo $item['id']?>"></a>
-                  <td><input type="submit" name = "remove" value="X"></td>
-                  <br>
-
-                <?php endforeach;?>
-
-
-
-            </tr>
-            <tr><td></td><td></td><td>Order Total: $21.98</td><td></td><td><a href="checkout.php"><input type=button value="Checkout"></a></td></tr>
-          </form>
-        </table>
-      </div>
-    </div>
-    <div class="page-right">
-    </div>
+    <table>
+      <tr>
+         <th> Name </th>
+         <th> Price </th>
+         <th> delete button </th>
+      </tr>
+    <?php
+    $total = 0;
+    foreach ($cart as $item) :?>
+      <tr class="item_row">
+        <form action="removeFromCart.php" method="post">
+            <td><?php $total = $total + $item['price']; ?></td>
+            <td> <?php echo $item['name']; ?></td>
+            <td> <?php echo $item['price']; ?></td>
+            <a><input type="hidden" name="itemId" value="<?php echo $item['itemId']?>"></a>
+            <td><input type="submit" name = "remove" value="X"></td>
+        </form>
+      </tr>
+    <?php endforeach;?>
+    </table>
   </div>
 
   <footer>
