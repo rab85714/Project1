f263413c6b53d950c187b554fd14e30556964f16
rab85714 <46548080+rab85714@users.noreply.github.com>
2021-12-02 16:48:11 -0500

    quantity stuff

--- updateQuantity.php
+++ updateQuantity.php
@@ -14,13 +14,17 @@
     if (isset($_POST['update'])) {
         $itemIdRaw = filter_input(INPUT_POST, 'itemId');
         $itemId = $itemIdRaw[0];
+        $itemQuantityRaw = filter_input(INPUT_POST, 'quantity');
+        $itemQuantity = $itemQuantityRaw[0];
         $email = $_SESSION['email'];
         $userQuery = "SELECT * FROM user WHERE email = :email";
         $userPrep = $db->prepare($userQuery);
         $userPrep->bindParam(':email', $email, PDO::PARAM_STR);
         $userId = $userPrep->execute();
         print "user id: " . $userId . "<br>";
-        print "item id" . $itemId . "<br>";
+        print "item id: " . $itemId . "<br>";
+        print "quantity: " . $itemQuantity . "<br>";
+
     }
 ?>
 </body>
