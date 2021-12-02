e02f2aef3a68a559c71c1f4bb988573c791f55de
alexschepers <ans91118@uga.edu>
2021-12-02 15:36:17 -0500

    Update login.php

--- login.php
+++ login.php
@@ -2,7 +2,7 @@
 
 <head>
   <meta charset="utf-8">
-  <title>Login - Bookstore</title>
+  <title>Mario's Bistro Login</title>
   <style>
     body {
       background: #E5E5E5;
@@ -16,8 +16,9 @@
     <div>
       <form action='loginProcess.php' method='post'>
         <div></div>
-        <h1>Log In</h1>
-        <div></div>
+        <h1>Welcome!</h1>
+        <h3>Log In</h3>
+
         <div>
           <span>email:</span>
           <input type="text" id="email" name="email" required autofocus></div>
@@ -41,24 +42,38 @@
 
 <script>
 function myFunction() {
-  // Get the value of the input field with id="numb"
-  let x = document.getElementById("email").value;
-  // If x is Not a Number or less than one or greater than 10
+
+  // set a text variable to be loaded into the page
   let text;
+
+  // get value of the email input
+  let x = document.getElementById("email").value;
+
+  //if email is blank, send this error
   if (x=="") {
     text = "Please enter your email.";
   }
 
+  // if email is invalid email format, send this error
+  var mailformat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+  if (!(x.match(mailformat)) {
+    text = "Please enter a valid email address.";
+  }
+
+  // get value of password input
   let y = document.getElementById("password").value;
 
+  // if password is blank, send this error
   if (y=="") {
     text = "Please enter your password.";
   }
+
 
   if(x=="" & y=="") {
     text = "Please enter you email and password.";
   }
 
+  // if text hasn't been assigned an error, leave it blank because no error!
   if(!text) {
     text ="";
   }
