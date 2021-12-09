c7f515f2b35c13c58c86f338236c8d1530093b4f
rab85714 <46548080+rab85714@users.noreply.github.com>
2021-12-09 15:38:34 -0500

    added input filter

--- checkoutProcess.php
+++ checkoutProcess.php
@@ -14,7 +14,7 @@
         if (isset($_POST['submitCheckout'])) {
             $radioLocation = filter_input(INPUT_POST, 'radioLocation');
             $selectedLocation = $radioLocation[0];
-            $radioPayment = filter_input(INPUT_POST, 'radioPayment');
+            $radioPayment = filter_input(INPUT_POST, 'radioPayment', FILTER_SANITIZE_STRING);
             $selectedPayment = $radioPayment[0];
 
             print "location s: " . $selectedLocation . "<br>";
