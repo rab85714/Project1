4e4e9c3353f20dc74363b4386bec02e60552e240
rab85714 <46548080+rab85714@users.noreply.github.com>
2021-12-09 17:01:23 -0500

    saving variables

--- checkoutProcess.php
+++ checkoutProcess.php
@@ -11,12 +11,15 @@
 </head>
 <body>
     <?php
+
         if (isset($_POST['submitCheckout'])) {
             if (isset($_POST['radioLocation'])) {
-                print "location: " . $_POST['radioLocation'] . "<br>";
+                $locationId = $_POST['radioLocation'];
+                print "location: " . $locationId . "<br>";
             }
             if (isset($_POST['radioPayment'])) {
-                print "payment: " . $_POST['radioPayment'] . "<br>";
+                $paymentName = $_POST['radioPayment'];
+                print "payment: " . $paymentName . "<br>";
             }
         }
     ?>
