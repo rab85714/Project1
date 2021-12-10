f6bca8cad0309e299824223074a46aabd1718247
rab85714 <46548080+rab85714@users.noreply.github.com>
2021-12-09 19:17:30 -0500

    added header

--- checkoutProcess.php
+++ checkoutProcess.php
@@ -20,7 +20,37 @@
 <html lang="en">
 
 <head>
+  <meta charset="utf-8">
+  <title>My Cart</title>
+  <link rel="stylesheet" href="index.css">
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" integrity="sha384-WskhaSGFgHYWDcbwN70/dfYBj47jz9qbsMId/iRN3ewGhXQFZCSftd1LZCfmhktB" crossorigin="anonymous">
+  <style>
+    .header {
+      font-family: 'Caveat';
+      font-size: 24px;
+    }
+
+    .options {
+      font-family: 'Ovo';
+      font-size: 22px;
+    }
+
+    .info {
+      font-family: 'Ovo';
+      font-size: 18px;
+    }
+  </style>
 </head>
+<header>
+<h1>Mario's Bistro</h1>
+  <nav>
+    <a class="link" href = "index.php"> Home </a>
+    <a class="link" href = "menu.php"> Menu </a>
+    <a class="link" href = "locations.php"> Locations </a>
+    <a class="link" href = "cart.php"> My Cart </a>
+    <a class="link" href="logout.php"> Logout </a>
+  </nav>
+</header>
 <body>
     <?php
         if (!isset($_POST['submitCheckout'])) {
